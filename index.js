d9c980bf325541aba07db2aa4a3a5cf98e9dd64c
6yw76ifyssgd4qxdzq874d1qvz44d4z <143006358+6yw76ifyssgd4qxdzq874d1qvz44d4z@users.noreply.github.com>
2023-08-24 01:23:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express')
 const app = express()
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+
+const users = ["shadow.no.money", "test"]
+const bannis = ["127.0.1.1"]
+
+app.get('/users', function (req, res) {
+    res.setHeader('Content-Type', 'text/json');
+    res.status(200).send(users)
+});
+
+app.get('/bannis', function (req, res) {
+    res.setHeader('Content-Type', 'text/json');
+    res.status(200).send(bannis)
+});
+
 app.listen(process.env.PORT || 3000)
